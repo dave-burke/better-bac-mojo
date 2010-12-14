a01a78a8d0abd4356a091453e0e68e65b4450876
dave <snewsoftware@gmail.com>
2010-12-13 21:42:41 -0600

    Added GPL comment to new files

--- src/app/assistants/help-man-entry-assistant.js
+++ src/app/assistants/help-man-entry-assistant.js
@@ -1,7 +1,20 @@
-/* this is the creator function for your scene assistant object. It will be passed all the 
-	additional parameters (after the scene name) that were passed to pushScene. The reference
-	to the scene controller (this.controller) has not be established yet, so any initialization
-	that needs the scene controller should be done in the setup function below. */
+/*
+ * This file is part of Better BAC
+ * Copyright (C) 2010 David Burke
+ *
+ * Better BAC is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * Better BAC is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
 function HelpManEntryAssistant(title, scene) {
 	this.title = title;
 	this.scene = scene;
